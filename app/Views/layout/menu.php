d37aa47445436ec6950749df45ecacc9f947de1b
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-10-31 08:34:08 +0700

    Create dynamic layout

---
+++ app/Views/layout/menu.php
@@ -0,0 +1,29 @@
+<li class="menu-header">Dashboard</li>
+<li class="dropdown">
+  <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Kode Akun</span></a>
+  <ul class="dropdown-menu">
+    <li><a class="nav-link" href="#">Akun - 1</a></li>
+    <li><a class="nav-link" href="#">Akun - 2</a></li>
+  </ul>
+</li>
+<li class="menu-header">Aktiviti</li>
+<li><a class="nav-link" href="#"><i class="fas fa-calendar-alt"></i> <span>Jurnal Umum</span></a></li>
+<li><a class="nav-link" href="#"><i class="fas fa-beer"></i> <span>Posting</span></a></li>
+<li><a class="nav-link" href="#"><i class="fas fa-balance-scale"></i> <span>Neraca Saldo</span></a></li>
+<li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Neraca Lajur</span></a></li>
+<li class="dropdown">
+  <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
+  <ul class="dropdown-menu">
+    <li><a class="nav-link" href="#">Transaksi Jurnal</a></li>
+    <li><a class="nav-link" href="#">Transaksi Penyesuaian</a></li>
+  </ul>
+</li>
+<li class="dropdown">
+  <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Laporan Keuangan</span></a>
+  <ul class="dropdown-menu">
+    <li><a class="nav-link" href="#">Laba Rugi</a></li>
+    <li><a class="nav-link" href="#">Perubahan Modal</a></li>
+    <li><a class="nav-link" href="#">Neraca</a></li>
+    <li><a class="nav-link" href="#">Arus Kas</a></li>
+  </ul>
+</li>
