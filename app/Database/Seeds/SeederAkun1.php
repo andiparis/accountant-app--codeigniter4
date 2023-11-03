50b17db5e67923af58d8c61d3b7290e9fe4a4acb
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-11-03 11:05:16 +0700

    Add seeder in Akun 1 & Akun 2

--- app/Database/Seeds/SeederAkun1.php
+++ app/Database/Seeds/SeederAkun1.php
@@ -8,6 +8,49 @@ class SeederAkun1 extends Seeder
 {
   public function run()
   {
-    //
+    $data = [
+      [
+        'kode_akun1'  => 11,
+        'nama_akun1'  => 'Aktiva Lancar',
+      ],
+      [
+        'kode_akun1'  => 12,
+        'nama_akun1'  => 'Aktiva Tetap',
+      ],
+      [
+        'kode_akun1'  => 21,
+        'nama_akun1'  => 'Hutang Jangka Pendek',
+      ],
+      [
+        'kode_akun1'  => 22,
+        'nama_akun1'  => 'Hutang Jangka Panjang',
+      ],
+      [
+        'kode_akun1'  => 31,
+        'nama_akun1'  => 'Modal Pemilik',
+      ],
+      [
+        'kode_akun1'  => 32,
+        'nama_akun1'  => 'Prive Pemilik',
+      ],
+      [
+        'kode_akun1'  => 41,
+        'nama_akun1'  => 'Pendapatan Usaha',
+      ],
+      [
+        'kode_akun1'  => 42,
+        'nama_akun1'  => 'Pendapatan di Luar Usaha',
+      ],
+      [
+        'kode_akun1'  => 51,
+        'nama_akun1'  => 'Beban Usaha',
+      ],
+      [
+        'kode_akun1'  => 52,
+        'nama_akun1'  => 'Beban di Luar Usaha',
+      ],
+    ];
+
+    $this->db->table('akun1s')->insertBatch($data);
   }
 }
--- app/Database/Seeds/SeederAkun2.php
+++ app/Database/Seeds/SeederAkun2.php
@@ -8,6 +8,144 @@ class SeederAkun2 extends Seeder
 {
   public function run()
   {
-    //
+    $data = [
+      [
+        'kode_akun2'  => 1101,
+        'nama_akun2'  => 'Kas',
+        'id_akun1'    => 1,
+      ],
+      [
+        'kode_akun2'  => 1102,
+        'nama_akun2'  => 'Piutang Usaha',
+        'id_akun1'    => 1,
+      ],
+      [
+        'kode_akun2'  => 1103,
+        'nama_akun2'  => 'Perlengkapan Kantor ',
+        'id_akun1'    => 1,
+      ],
+      [
+        'kode_akun2'  => 1104,
+        'nama_akun2'  => 'Sewa Dibayar Dimuka',
+        'id_akun1'    => 1,
+      ],
+      [
+        'kode_akun2'  => 1105,
+        'nama_akun2'  => 'Asuransi Dibayar Dimuka',
+        'id_akun1'    => 1,
+      ],
+      [
+        'kode_akun2'  => 1201,
+        'nama_akun2'  => 'Peralatan Kantor',
+        'id_akun1'    => 2,
+      ],
+      [
+        'kode_akun2'  => 1202,
+        'nama_akun2'  => 'Akumulasi Penyusutan P.Kantor',
+        'id_akun1'    => 2,
+      ],
+      [
+        'kode_akun2'  => 1203,
+        'nama_akun2'  => 'Tanah',
+        'id_akun1'    => 2,
+      ],
+      [
+        'kode_akun2'  => 2101,
+        'nama_akun2'  => 'Hutang Usaha',
+        'id_akun1'    => 3,
+      ],
+      [
+        'kode_akun2'  => 2102,
+        'nama_akun2'  => 'Hutang Gaji',
+        'id_akun1'    => 3,
+      ],
+      [
+        'kode_akun2'  => 2103,
+        'nama_akun2'  => 'Pendapatan Diterima Dimuka',
+        'id_akun1'    => 3,
+      ],
+      [
+        'kode_akun2'  => 2201,
+        'nama_akun2'  => 'Hutang Hipotek',
+        'id_akun1'    => 4,
+      ],
+      [
+        'kode_akun2'  => 2202,
+        'nama_akun2'  => 'Hutang Obligasi',
+        'id_akun1'    => 4,
+      ],
+      [
+        'kode_akun2'  => 3101,
+        'nama_akun2'  => 'Modal Pemilik',
+        'id_akun1'    => 5,
+      ],
+      [
+        'kode_akun2'  => 3201,
+        'nama_akun2'  => 'Prive Tuan Najwan',
+        'id_akun1'    => 6,
+      ],
+      [
+        'kode_akun2'  => 4101,
+        'nama_akun2'  => 'Pendapatan Jasa',
+        'id_akun1'    => 7,
+      ],
+      [
+        'kode_akun2'  => 4102,
+        'nama_akun2'  => 'Pendapatan Diterima Dimuka',
+        'id_akun1'    => 7,
+      ],
+      [
+        'kode_akun2'  => 4201,
+        'nama_akun2'  => 'Pendapatan Diluar Usaha',
+        'id_akun1'    => 8,
+      ],
+      [
+        'kode_akun2'  => 5101,
+        'nama_akun2'  => 'Beban Gaji Karyawan',
+        'id_akun1'    => 9,
+      ],
+      [
+        'kode_akun2'  => 5102,
+        'nama_akun2'  => 'Beban Iklan',
+        'id_akun1'    => 9,
+      ],
+      [
+        'kode_akun2'  => 5103,
+        'nama_akun2'  => 'Beban Asuransi',
+        'id_akun1'    => 9,
+      ],
+      [
+        'kode_akun2'  => 5104,
+        'nama_akun2'  => 'Beban Telepon',
+        'id_akun1'    => 9,
+      ],
+      [
+        'kode_akun2'  => 5105,
+        'nama_akun2'  => 'Beban Listrik',
+        'id_akun1'    => 9,
+      ],
+      [
+        'kode_akun2'  => 5106,
+        'nama_akun2'  => 'Beban Sewa',
+        'id_akun1'    => 9,
+      ],
+      [
+        'kode_akun2'  => 5107,
+        'nama_akun2'  => 'Beban Penyusutan Peralatan Kantor',
+        'id_akun1'    => 9,
+      ],
+      [
+        'kode_akun2'  => 5108,
+        'nama_akun2'  => 'Beban Perlengkapan Kantor',
+        'id_akun1'    => 9,
+      ],
+      [
+        'kode_akun2'  => 5201,
+        'nama_akun2'  => 'Beban Bunga',
+        'id_akun1'    => 10,
+      ],
+    ];
+
+    $this->db->table('akun2s')->insertBatch($data);
   }
 }
