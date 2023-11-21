b2ab7fca00668c2ee77d76fd454de94334a79609
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-11-21 19:08:17 +0700

    Created model for user menu

---
+++ app/Models/ModelUser.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ModelUser extends Model
+{
+  // protected $useAutoIncrement = true;
+  // protected $useSoftDeletes   = false;
+  // protected $protectFields    = true;
+  protected $table            = 'users';
+  protected $primaryKey       = 'id';
+  protected $returnType       = 'object';
+  protected $allowedFields    = ['email', 'username', 'fullname', 'password_hash', 'active'];
+
+  // Dates
+  protected $useTimestamps = true;
+  // protected $dateFormat    = 'datetime';
+  // protected $createdField  = 'created_at';
+  // protected $updatedField  = 'updated_at';
+  // protected $deletedField  = 'deleted_at';
+
+  // Validation
+  // protected $validationRules      = [];
+  // protected $validationMessages   = [];
+  // protected $skipValidation       = false;
+  // protected $cleanValidationRules = true;
+
+  // Callbacks
+  // protected $allowCallbacks = true;
+  // protected $beforeInsert   = [];
+  // protected $afterInsert    = [];
+  // protected $beforeUpdate   = [];
+  // protected $afterUpdate    = [];
+  // protected $beforeFind     = [];
+  // protected $afterFind      = [];
+  // protected $beforeDelete   = [];
+  // protected $afterDelete    = [];
+
+  public function getAllUsers()
+  {
+    $query = $this->db->table('users AS usr')
+      ->join('auth_groups_users AS grp_usr', 'grp_usr.user_id = usr.id', 'left')
+      ->join('auth_groups AS grp', 'grp.id = grp_usr.group_id', 'left')
+      ->select('usr.id, email, username, fullname, name AS role');
+
+    return $query->get()->getResultObject();
+  }
+
+  public function getAllRoles()
+  {
+    $query = $this->db->table('auth_groups');
+    return $query->get()->getResultObject();
+  }
+
+  public function getUserById($id)
+  {
+    $query = $this->db->table('users AS usr')
+      ->join('auth_groups_users AS grp_usr', 'grp_usr.user_id = usr.id')
+      ->select('id, email, username, fullname, group_id')
+      ->where('usr.id', $id);
+
+    return $query->get()->getRowObject();
+  }
+}
