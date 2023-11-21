ea6ddd284b2a87f4de8fde54f3b797b600657e3d
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-11-21 19:14:17 +0700

    Added user permission

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,40 +5,48 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Home::index', ['filter' => 'role:user,admin,manajer,direktur']);
 
 // Akun 1
-$routes->get('/akun1', 'Akun1::index');
-$routes->get('/akun1/new', 'Akun1::new');
-$routes->get('/akun1/edit/(:any)', 'Akun1::edit/$1');
-$routes->post('/akun1', 'Akun1::store');
-$routes->put('/akun1/edit/(:any)', 'Akun1::update/$1');
-$routes->delete('/akun1/(:any)', 'Akun1::destroy/$1');
+$routes->get('/akun1', 'Akun1::index', ['filter' => 'role:admin']);
+$routes->get('/akun1/index', 'Akun1::index', ['filter' => 'role:admin']);
+$routes->get('/akun1/new', 'Akun1::new', ['filter' => 'role:admin']);
+$routes->get('/akun1/edit/(:any)', 'Akun1::edit/$1', ['filter' => 'role:admin']);
+$routes->post('/akun1', 'Akun1::store', ['filter' => 'role:admin']);
+$routes->put('/akun1/edit/(:any)', 'Akun1::update/$1', ['filter' => 'role:admin']);
+$routes->delete('/akun1/(:any)', 'Akun1::destroy/$1', ['filter' => 'role:admin']);
 
 // Akun 2
-$routes->resource('akun2');
+$routes->resource('akun2', ['filter' => 'role:admin']);
 
 // Transaksi
-$routes->get('/transaksi/akun2', 'Transaksi::akun2');
-$routes->get('/transaksi/status', 'Transaksi::status');
-$routes->resource('transaksi');
+$routes->get('/transaksi/akun2', 'Transaksi::akun2', ['filter' => 'role:admin']);
+$routes->get('/transaksi/status', 'Transaksi::status', ['filter' => 'role:admin']);
+$routes->resource('transaksi', ['filter' => 'role:admin']);
 
 // Jurnal Umum
-$routes->get('/jurnalumum', 'JurnalUmum::index');
-$routes->post('/jurnalumum', 'JurnalUmum::index');
-$routes->post('/jurnalumum/printjurnalumum', 'JurnalUmum::printjurnalumum');
+$routes->get('/jurnalumum', 'JurnalUmum::index', ['filter' => 'role:user,admin']);
+$routes->get('/jurnalumum/index', 'JurnalUmum::index', ['filter' => 'role:user,admin']);
+$routes->post('/jurnalumum', 'JurnalUmum::index', ['filter' => 'role:user,admin']);
+$routes->post('/jurnalumum/printjurnalumum', 'JurnalUmum::printjurnalumum', ['filter' => 'role:admin']);
 
 // Posting
-$routes->get('/posting', 'Posting::index');
-$routes->post('/posting', 'Posting::index');
-$routes->post('/posting/printposting', 'Posting::printposting');
+$routes->get('/posting', 'Posting::index', ['filter' => 'role:admin']);
+$routes->get('/posting/index', 'Posting::index', ['filter' => 'role:admin']);
+$routes->post('/posting', 'Posting::index', ['filter' => 'role:admin']);
+$routes->post('/posting/printposting', 'Posting::printposting', ['filter' => 'role:admin']);
 
 // Neraca Saldo
-$routes->get('/neracasaldo', 'NeracaSaldo::index');
-$routes->post('/neracasaldo', 'NeracaSaldo::index');
-$routes->post('/neracasaldo/printneracasaldo', 'NeracaSaldo::printneracasaldo');
+$routes->get('/neracasaldo', 'NeracaSaldo::index', ['filter' => 'role:direktur']);
+$routes->get('/neracasaldo/index', 'NeracaSaldo::index', ['filter' => 'role:direktur']);
+$routes->post('/neracasaldo', 'NeracaSaldo::index', ['filter' => 'role:direktur']);
+$routes->post('/neracasaldo/printneracasaldo', 'NeracaSaldo::printneracasaldo', ['filter' => 'role:direktur']);
 
 // Laba Rugi
-$routes->get('/labarugi', 'LabaRugi::index');
-$routes->post('/labarugi', 'LabaRugi::index');
-$routes->post('/labarugi/printlabarugi', 'LabaRugi::printlabarugi');
+$routes->get('/labarugi', 'LabaRugi::index', ['filter' => 'role:direktur']);
+$routes->get('/labarugi/index', 'LabaRugi::index', ['filter' => 'role:direktur']);
+$routes->post('/labarugi', 'LabaRugi::index', ['filter' => 'role:direktur']);
+$routes->post('/labarugi/printlabarugi', 'LabaRugi::printlabarugi', ['filter' => 'role:direktur']);
+
+// User
+$routes->resource('user', ['filter' => 'role:manajer']);
--- app/Views/layout/menu.php
+++ app/Views/layout/menu.php
@@ -1,25 +1,50 @@
-<li class="menu-header">Dashboard</li>
-<li class="dropdown">
-  <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Kode Akun</span></a>
-  <ul class="dropdown-menu">
-    <li><a class="nav-link" href="<?= site_url('akun1') ?>">Akun - 1</a></li>
-    <li><a class="nav-link" href="<?= site_url('akun2') ?>">Akun - 2</a></li>
-  </ul>
-</li>
-<li class="menu-header">Aktiviti</li>
-<li><a class="nav-link" href="<?= site_url('jurnalumum') ?>"><i class="fas fa-calendar-alt"></i> <span>Jurnal Umum</span></a></li>
-<li><a class="nav-link" href="<?= site_url('posting') ?>"><i class="fas fa-beer"></i> <span>Posting</span></a></li>
-<li><a class="nav-link" href="<?= site_url('neracasaldo') ?>"><i class="fas fa-balance-scale"></i> <span>Neraca Saldo</span></a></li>
-<li class="dropdown">
-  <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Transaksi</span></a>
-  <ul class="dropdown-menu">
-    <li><a class="nav-link" href="<?= site_url('transaksi') ?>">Transaksi Jurnal</a></li>
-  </ul>
-</li>
-<li class="dropdown">
-  <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Laporan Keuangan</span></a>
-  <ul class="dropdown-menu">
-    <li><a class="nav-link" href="<?= site_url('labarugi') ?>">Laba Rugi</a></li>
-    <li><a class="nav-link" href="#">Arus Kas</a></li>
-  </ul>
-</li>
+<?php if (in_groups(['user', 'admin', 'manajer', 'direktur'])) { ?>
+  <li class="menu-header">Dashboard</li>
+  <li><a class="nav-link" href="<?= site_url('home') ?>"><i class="fas fa-home"></i> <span>Home</span></a></li>
+<?php } ?>
+
+<?php if (in_groups(['manajer'])) { ?>
+  <li><a class="nav-link" href="<?= site_url('user') ?>"><i class="fas fa-users"></i> <span>User</span></a></li>
+<?php } ?>
+
+<?php if (in_groups(['admin'])) { ?>
+  <li class="dropdown">
+    <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Kode Akun</span></a>
+    <ul class="dropdown-menu">
+      <li><a class="nav-link" href="<?= site_url('akun1') ?>">Akun - 1</a></li>
+      <li><a class="nav-link" href="<?= site_url('akun2') ?>">Akun - 2</a></li>
+    </ul>
+  </li>
+<?php } ?>
+
+<?php if (in_groups(['user', 'admin', 'direktur'])) { ?>
+  <li class="menu-header">Aktiviti</li>
+<?php } ?>
+
+<?php if (in_groups(['admin'])) { ?>
+  <li class="dropdown">
+    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Transaksi</span></a>
+    <ul class="dropdown-menu">
+      <li><a class="nav-link" href="<?= site_url('transaksi') ?>">Transaksi Jurnal</a></li>
+    </ul>
+  </li>
+<?php } ?>
+
+<?php if (in_groups(['user', 'admin'])) { ?>
+  <li><a class="nav-link" href="<?= site_url('jurnalumum') ?>"><i class="fas fa-calendar-alt"></i> <span>Jurnal Umum</span></a></li>
+<?php } ?>
+
+<?php if (in_groups(['admin'])) { ?>
+  <li><a class="nav-link" href="<?= site_url('posting') ?>"><i class="fas fa-beer"></i> <span>Posting</span></a></li>
+<?php } ?>
+
+<?php if (in_groups(['direktur'])) { ?>
+  <li><a class="nav-link" href="<?= site_url('neracasaldo') ?>"><i class="fas fa-balance-scale"></i> <span>Neraca Saldo</span></a></li>
+  <li class="dropdown">
+    <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Laporan Keuangan</span></a>
+    <ul class="dropdown-menu">
+      <li><a class="nav-link" href="<?= site_url('labarugi') ?>">Laba Rugi</a></li>
+      <li><a class="nav-link" href="#">Arus Kas</a></li>
+    </ul>
+  </li>
+<?php } ?>
