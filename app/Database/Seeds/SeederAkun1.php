fc242b541d0a7d4d6fb7d422dcd63e734c9ea687
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-19 12:18:25 +0700

    Update akun1 seeder

--- app/Database/Seeds/SeederAkun1.php
+++ app/Database/Seeds/SeederAkun1.php
@@ -10,44 +10,54 @@ class SeederAkun1 extends Seeder
   {
     $data = [
       [
-        'kode_akun1'  => 11,
+        'kode_akun1'  => 1100,
         'nama_akun1'  => 'Aktiva Lancar',
+        'jenis_akun'  => 1,
       ],
       [
-        'kode_akun1'  => 12,
+        'kode_akun1'  => 1200,
         'nama_akun1'  => 'Aktiva Tetap',
+        'jenis_akun'  => 1,
       ],
       [
-        'kode_akun1'  => 21,
+        'kode_akun1'  => 2100,
         'nama_akun1'  => 'Hutang Jangka Pendek',
+        'jenis_akun'  => 2,
       ],
       [
-        'kode_akun1'  => 22,
+        'kode_akun1'  => 2200,
         'nama_akun1'  => 'Hutang Jangka Panjang',
+        'jenis_akun'  => 2,
       ],
       [
-        'kode_akun1'  => 31,
+        'kode_akun1'  => 3100,
         'nama_akun1'  => 'Modal Pemilik',
+        'jenis_akun'  => 3,
       ],
       [
-        'kode_akun1'  => 32,
+        'kode_akun1'  => 3200,
         'nama_akun1'  => 'Prive Pemilik',
+        'jenis_akun'  => 3,
       ],
       [
-        'kode_akun1'  => 41,
+        'kode_akun1'  => 4100,
         'nama_akun1'  => 'Pendapatan Usaha',
+        'jenis_akun'  => 4,
       ],
       [
-        'kode_akun1'  => 42,
+        'kode_akun1'  => 4200,
         'nama_akun1'  => 'Pendapatan di Luar Usaha',
+        'jenis_akun'  => 4,
       ],
       [
-        'kode_akun1'  => 51,
+        'kode_akun1'  => 5100,
         'nama_akun1'  => 'Beban Usaha',
+        'jenis_akun'  => 5,
       ],
       [
-        'kode_akun1'  => 52,
+        'kode_akun1'  => 5200,
         'nama_akun1'  => 'Beban di Luar Usaha',
+        'jenis_akun'  => 5,
       ],
     ];
 
