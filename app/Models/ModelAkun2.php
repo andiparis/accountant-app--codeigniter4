c58f1afde646679e9706d34f01748b7218c312d6
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-19 15:19:49 +0700

    Add new functions for get akun data

--- app/Models/ModelAkun2.php
+++ app/Models/ModelAkun2.php
@@ -45,4 +45,43 @@ class ModelAkun2 extends Model
     $query = $builder->get();
     return $query->getResult();
   }
+
+  function getAkun1()
+  {
+    $query = $this->db->table('akun1s')
+      ->orderBy('kode_akun1');
+
+    return $query->get()->getResult();
+  }
+
+  function getAkun($accountType)
+  {
+    $query = [];
+
+    foreach ($accountType as $key => $value) {
+      $query[$key] = $this->db->query(
+        "SELECT akun2s.kode_akun2 AS kode_akun, akun2s.nama_akun2 AS nama_akun, jenis_akun = null AS jenis_akun
+        FROM akun1s
+        JOIN akun2s ON akun1s.id_akun1 = akun2s.id_akun1
+        WHERE akun1s.jenis_akun = ($key + 1)
+  
+        UNION
+  
+        SELECT kode_akun1 AS kode_akun, nama_akun1 AS nama_akun, jenis_akun
+        FROM akun1s
+        WHERE jenis_akun = ($key + 1)
+        ORDER BY kode_akun"
+      )->getResult();
+    }
+
+    return $query;
+  }
+
+  function getAkunById($id, $table, $tableField)
+  {
+    $query = $this->db->table($table)
+      ->where($tableField, $id);
+
+    return $query->get()->getRow();
+  }
 }
