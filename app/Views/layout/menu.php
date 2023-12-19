3c0767d844d1915a09127e8c5d0481281340a087
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-19 11:47:17 +0700

    Delete akun1 menu

--- app/Views/layout/menu.php
+++ app/Views/layout/menu.php
@@ -1,20 +1,14 @@
 <?php if (in_groups(['user', 'admin', 'manajer', 'direktur'])) { ?>
   <li class="menu-header">Dashboard</li>
-  <li><a class="nav-link" href="<?= site_url('home') ?>"><i class="fas fa-home"></i> <span>Home</span></a></li>
+  <li><a class="nav-link" href="<?= site_url('home') ?>"><i class="fas fa-home"></i><span>Home</span></a></li>
 <?php } ?>
 
 <?php if (in_groups(['manajer'])) { ?>
-  <li><a class="nav-link" href="<?= site_url('user') ?>"><i class="fas fa-users"></i> <span>User</span></a></li>
+  <li><a class="nav-link" href="<?= site_url('user') ?>"><i class="fas fa-users"></i><span>User</span></a></li>
 <?php } ?>
 
 <?php if (in_groups(['admin'])) { ?>
-  <li class="dropdown">
-    <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Kode Akun</span></a>
-    <ul class="dropdown-menu">
-      <li><a class="nav-link" href="<?= site_url('akun1') ?>">Akun - 1</a></li>
-      <li><a class="nav-link" href="<?= site_url('akun2') ?>">Akun - 2</a></li>
-    </ul>
-  </li>
+  <li><a href="<?= site_url('akun2') ?>" class="nav-link"><i class="fas fa-fire"></i><span>Kode Akun</span></a></li>
 <?php } ?>
 
 <?php if (in_groups(['user', 'admin', 'direktur'])) { ?>
@@ -23,7 +17,7 @@
 
 <?php if (in_groups(['admin'])) { ?>
   <li class="dropdown">
-    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Transaksi</span></a>
+    <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i><span>Transaksi</span></a>
     <ul class="dropdown-menu">
       <li><a class="nav-link" href="<?= site_url('transaksi') ?>">Transaksi Jurnal</a></li>
     </ul>
@@ -31,17 +25,17 @@
 <?php } ?>
 
 <?php if (in_groups(['user', 'admin'])) { ?>
-  <li><a class="nav-link" href="<?= site_url('jurnalumum') ?>"><i class="fas fa-calendar-alt"></i> <span>Jurnal Umum</span></a></li>
+  <li><a class="nav-link" href="<?= site_url('jurnalumum') ?>"><i class="fas fa-calendar-alt"></i><span>Jurnal Umum</span></a></li>
 <?php } ?>
 
 <?php if (in_groups(['admin'])) { ?>
-  <li><a class="nav-link" href="<?= site_url('posting') ?>"><i class="fas fa-beer"></i> <span>Posting</span></a></li>
+  <li><a class="nav-link" href="<?= site_url('posting') ?>"><i class="fas fa-beer"></i><span>Buku Besar</span></a></li>
 <?php } ?>
 
 <?php if (in_groups(['direktur'])) { ?>
-  <li><a class="nav-link" href="<?= site_url('neracasaldo') ?>"><i class="fas fa-balance-scale"></i> <span>Neraca Saldo</span></a></li>
+  <li><a class="nav-link" href="<?= site_url('neracasaldo') ?>"><i class="fas fa-balance-scale"></i><span>Neraca Saldo</span></a></li>
   <li class="dropdown">
-    <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Laporan Keuangan</span></a>
+    <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i><span>Laporan Keuangan</span></a>
     <ul class="dropdown-menu">
       <li><a class="nav-link" href="<?= site_url('labarugi') ?>">Laba Rugi</a></li>
       <li><a class="nav-link" href="<?= site_url('aruskas') ?>">Arus Kas</a></li>
