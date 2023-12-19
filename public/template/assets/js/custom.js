032ba5fa9b33edbd9c77dfda71b95d62bc0f20cc
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-19 11:45:41 +0700

    Repair js when add or edit akun

--- public/template/assets/js/custom.js
+++ public/template/assets/js/custom.js
@@ -12,6 +12,30 @@ function deleteData(id) {
   $("#delete-" + id).submit();
 }
 
+// Show or hide parent account selector
+const statusAkun = document.getElementById("status_akun");
+
+function toggleCodeBlock() {
+  const accountType = document.getElementById("accountType");
+  const parentAccountName = document.getElementById("parentAccountName");
+
+  if (statusAkun === null || statusAkun.value === "1") {
+    accountType.style.display = "block";
+    parentAccountName.style.display = "none";
+  } else {
+    accountType.style.display = "none";
+    parentAccountName.style.display = "block";
+  }
+}
+
+if (statusAkun !== null) {
+  document
+    .getElementById("status_akun")
+    .addEventListener("change", toggleCodeBlock);
+}
+
+toggleCodeBlock();
+
 // Dynamic menu
 let path = location.pathname.split("/");
 let url = location.origin + "/" + path[1];
