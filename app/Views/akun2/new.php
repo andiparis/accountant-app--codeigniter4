ee17f17aeaef9f2b3cd1a07680323134fa57083d
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-19 15:22:25 +0700

    Add some new form field

--- app/Views/akun2/new.php
+++ app/Views/akun2/new.php
@@ -1,7 +1,7 @@
 <?= $this->extend('layout/backend') ?>
 
 <?= $this->section('title') ?>
-<title>SIA &mdash; Add Akun 2</title>
+<title>SIA &mdash; Add Akun</title>
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
@@ -14,27 +14,43 @@
   <div class="section-body">
     <div class="card">
       <div class="card-header">
-        <h4>Tambah Data Akun 2</h4>
+        <h4>Tambah Data Akun</h4>
       </div>
       <div class="card-body p-4">
         <form action="<?= site_url('akun2') ?>" method="post">
           <?= csrf_field() ?>
           <div class="form-group">
-            <label for="id_akun1">Nama Akun 1</label>
-            <select name="id_akun1" id="id_akun1" class="form-control" required>
-              <option value="">- Pilih nama akun 1 -</option>
+            <label for="status_akun">Status Akun</label>
+            <select name="status_akun" id="status_akun" class="form-control" required>
+              <option value="1" selected>Parent</option>
+              <option value="2">Child</option>
+            </select>
+          </div>
+          <div id="accountType" class="form-group">
+            <label for="jenis_akun">Jenis Akun</label>
+            <select name="jenis_akun" id="jenis_akun" class="form-control" required>
+              <option value="1" selected>Aktiva</option>
+              <option value="2">Hutang</option>
+              <option value="3">Modal</option>
+              <option value="4">Pendapatan</option>
+              <option value="5">Beban</option>
+            </select>
+          </div>
+          <div id="parentAccountName" class="form-group">
+            <label for="id_akun_parent">Nama Akun Parent</label>
+            <select name="id_akun_parent" id="id_akun_parent" class="form-control" required>
               <?php foreach ($akun1Data as $key => $akun1) { ?>
                 <option value="<?= $akun1->id_akun1 ?>"><?= $akun1->nama_akun1 ?></option>
               <?php } ?>
             </select>
           </div>
           <div class="form-group">
-            <label for="kode_akun2">Kode Akun 2</label>
-            <input type="text" name="kode_akun2" id="kode_akun2" placeholder="Kode akun 2" class="form-control" required>
+            <label for="kode_akun">Kode Akun</label>
+            <input type="text" name="kode_akun" id="kode_akun" placeholder="Kode akun" class="form-control" required>
           </div>
           <div class="form-group">
-            <label for="nama_akun2">Nama Akun 2</label>
-            <input type="text" name="nama_akun2" id="nama_akun2" placeholder="Nama akun 2" class="form-control" required>
+            <label for="nama_akun">Nama Akun</label>
+            <input type="text" name="nama_akun" id="nama_akun" placeholder="Nama akun" class="form-control" required>
           </div>
           <div class="form-group">
             <button type="submit" class="btn btn-success"><i class="fas fa-paper-plane"></i> Save</button>
