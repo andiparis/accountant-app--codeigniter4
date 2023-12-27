251884d2717d6ba1b4733322af2d36bc6a0402e4
Andi Paris Bachtiar <andiparis02@gmail.com>
2023-12-27 21:15:32 +0700

    Fix an error in the js

--- public/template/assets/js/custom.js
+++ public/template/assets/js/custom.js
@@ -12,30 +12,6 @@ function deleteData(id) {
   $("#delete-" + id).submit();
 }
 
-// Show or hide parent account selector
-const statusAkun = document.getElementById("status_akun");
-
-function toggleCodeBlock() {
-  const accountType = document.getElementById("accountType");
-  const parentAccountName = document.getElementById("parentAccountName");
-
-  if (statusAkun === null || statusAkun.value === "1") {
-    accountType.style.display = "block";
-    parentAccountName.style.display = "none";
-  } else {
-    accountType.style.display = "none";
-    parentAccountName.style.display = "block";
-  }
-}
-
-if (statusAkun !== null) {
-  document
-    .getElementById("status_akun")
-    .addEventListener("change", toggleCodeBlock);
-}
-
-toggleCodeBlock();
-
 // Dynamic menu
 let path = location.pathname.split("/");
 let url = location.origin + "/" + path[1];
@@ -151,3 +127,27 @@ function formSelectStatus(nomor) {
     $("#id_status" + nomor).html(output.join(""));
   });
 }
+
+// Show or hide parent account selector
+const statusAkun = document.getElementById("status_akun");
+
+function toggleCodeBlock() {
+  const accountType = document.getElementById("accountType");
+  const parentAccountName = document.getElementById("parentAccountName");
+
+  if (statusAkun === null || statusAkun.value === "1") {
+    accountType.style.display = "block";
+    parentAccountName.style.display = "none";
+  } else {
+    accountType.style.display = "none";
+    parentAccountName.style.display = "block";
+  }
+}
+
+if (statusAkun !== null) {
+  document
+    .getElementById("status_akun")
+    .addEventListener("change", toggleCodeBlock);
+}
+
+toggleCodeBlock();
